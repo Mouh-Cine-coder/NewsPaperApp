283dad09607240dddc9098cb3bd99bec3d347798
Mouh-Cine-coder <mouhcinedafiaa@gmail.com>
2023-02-19 01:34:55 +0100

    add crispy

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+# crispy configuration
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" 
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
